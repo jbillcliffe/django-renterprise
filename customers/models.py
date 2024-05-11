84d2c528934c11b3cb4d180850851a648098ef71
jbillcliffe <jbillcliffe@googlemail.com>
2024-05-11 12:51:56 +0000

    (feat) add customer comment model

--- customers/models.py
+++ customers/models.py
@@ -1,12 +1,22 @@
 from django.db import models
-from django.db.models import Model
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Customer(Model):
+class Customer(models.Model):
     unique_id = models.UUIDField()
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     address = models.TextField()
     postcode = models.CharField(max_length=8)
     created_on = models.DateTimeField(auto_now_add=True)
+
+class CustomerNote(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.PROTECT, related_name="customer"
+    )
+    note = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="created_by"
+    )
