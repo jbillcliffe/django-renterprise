3f8319c3f4665101bb6b3279c0c7351f2485f7be
jbillcliffe <jbillcliffe@googlemail.com>
2024-05-11 11:54:45 +0000

    (feat) customer model created

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
+
+class Customer(Model):
+    unique_id = models.UUIDField()
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    address = models.TextField()
+    postcode = models.CharField(max_length=8)
+    created_on = models.DateTimeField(auto_now_add=True)
