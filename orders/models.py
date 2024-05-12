794a2ade05bb6668f1bf7abbea3b79c5a6efefcb
jbillcliffe <jbillcliffe@googlemail.com>
2024-05-12 02:29:26 +0000

    (feat) create Order and OrderNote models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,36 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+from items import Item
+from customers import Customer
 
 # Create your models here.
+class Order(models.Model):
+    order_id = models.UUIDField(
+        primary_key=False,
+        default=uuid.uuid4,
+        editable=False)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="customer"
+    )
+    item = models.ForeignKey(
+        Item, on_delete=models.PROTECT, related_name="item"
+    )
+    cost_initial = models.DecimalField(max_digits=6, decimal_places=2)
+    cost_week = models.DecimalField(max_digits=6, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="created_by"
+    )
+
+class OrderNote(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="order"
+    )
+    note = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="created_by"
+    )
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def main_menu(request):
+    """
+    Function to display the main menu at the root
+    of the project
+    """
+    return HttpResponse("Hello, Orders!")
