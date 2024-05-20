e87338e8e7c3688cf5eb58bc16186c2efe939b42
jbillcliffe <jbillcliffe@googlemail.com>
2024-05-20 09:11:59 +0000

    bugfix/issue with naming convention of image

--- items/models.py
+++ items/models.py
@@ -37,8 +37,3 @@ class Item(models.Model):
 
     def item_type_category(self):
         return f"{self.item_type.category}"
-
-    def item_image(self):
-        return f"{self.item_type.image}"
-    
-    print(item_image)
